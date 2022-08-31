9b12656ba32d942ab91d46c300cc969332748b31
abdallahOmarAhmed <abdallah.omar.gazaly@gmail.com>
2022-08-31 16:50:11 +0200

    remove bias

--- deeplapv3/utils.py
+++ deeplapv3/utils.py
@@ -3,10 +3,10 @@ from torch import nn
 
 
 class Pooling(nn.Module):
-    def __init__(self, in_ch, out_ch, img_size, bias):
+    def __init__(self, in_ch, out_ch, img_size):
         super().__init__()
         self.pool = nn.AdaptiveAvgPool2d(1)
-        self.conv = nn.Conv2d(in_ch, out_ch, 1, bias=bias)
+        self.conv = nn.Conv2d(in_ch, out_ch, 1, bias=False)
         self.bn = nn.BatchNorm2d(out_ch)
         self.relu = nn.ReLU(inplace=True)
         self.up = nn.UpsamplingBilinear2d(img_size)
@@ -20,21 +20,22 @@ class ASPP(nn.Module):
         super(ASPP, self).__init__()
         self.relu = nn.ReLU(inplace=True)
 
-        self.conv1 = nn.Conv2d(in_ch, out_ch, 1)
+        self.conv1 = nn.Conv2d(in_ch, out_ch, 1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_ch)
 
-        self.conv2 = nn.Conv2d(in_ch, out_ch, 3, dilation=6, padding='same')
+        self.conv2 = nn.Conv2d(in_ch, out_ch, 3, dilation=6, padding='same', bias=False)
         self.bn2 = nn.BatchNorm2d(out_ch)
 
-        self.conv3 = nn.Conv2d(in_ch, out_ch, 3, dilation=12, padding='same')
+        self.conv3 = nn.Conv2d(in_ch, out_ch, 3, dilation=12, padding='same', bias=False)
         self.bn3 = nn.BatchNorm2d(out_ch)
 
-        self.conv4 = nn.Conv2d(in_ch, out_ch, 3, dilation=18, padding='same')
+        self.conv4 = nn.Conv2d(in_ch, out_ch, 3, dilation=18, padding='same', bias=False)
         self.bn4 = nn.BatchNorm2d(out_ch)
 
         self.pool = Pooling(in_ch, out_ch, img_size)
 
-        self.conv_end = nn.Conv2d(out_ch*5, out_ch, 1)
+        self.conv_end = nn.Conv2d(out_ch*5, out_ch, 1, bias=False)
+        self.conv_end = nn.Conv2d(out_ch*5, out_ch, 1, bias=False)
         self.bn_end = nn.BatchNorm2d(out_ch)
 
     def forward(self, x):
