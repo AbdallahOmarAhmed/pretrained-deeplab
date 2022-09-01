330894015c10ab1b26a9f7e4c789802919202b3e
abdallahOmarAhmed <abdallah.omar.gazaly@gmail.com>
2022-09-01 14:52:53 +0200

    add decoder

--- deeplab/deeplab_v3plus.py
+++ deeplab/deeplab_v3plus.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 from utils import *
@@ -16,3 +17,25 @@ class Encoder(nn.Module):
         res, x = self.backbone(x)
         x = self.aspp(x)
         return res, x
+
+
+class Decoder(nn.Module):
+    def __init__(self, num_classes, in_ch, reduction=4):
+        super().__init__()
+        out_ch = in_ch[1]//reduction
+        self.relu = nn.ReLU(inplace=True)
+        self.up = nn.UpsamplingBilinear2d(scale_factor=4)
+
+        self.reduce = nn.Conv2d(in_ch[0], out_ch, 1, bias=False)
+        self.reduce_bn = nn.BatchNorm2d(out_ch)
+
+        self.cat_conv = nn.Conv2d(in_ch[1]+out_ch, in_ch[1], 3, bias=False, padding=1)
+        self.cat_bn = nn.BatchNorm2d(in_ch[1])
+
+        self.end_conv = nn.Conv2d(in_ch[1], num_classes, 1)
+
+    def forward(self, res, x):
+        out = torch.cat((self.up(x), self.reduce_bn(self.reduce(res))), dim=1)
+        out = self.up(self.cat_bn(self.cat_conv(out)))
+        return self.end_conv(out)
+
